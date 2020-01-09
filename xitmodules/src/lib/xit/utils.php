955e04785c9763b39b9b2c604ce4bc6ad369e85c
derekmwirigi <derekmwirigi@gmail.com>
2020-01-09 23:18:23 +0300

    Utils

--- xitmodules/src/lib/xit/utils.php
+++ xitmodules/src/lib/xit/utils.php
@@ -7,4 +7,12 @@
         public function createToken (){
             return sha1(sha1(md5(uniqid())));
         }
+
+        public function generateRandom($Start, $End, $Length){
+            $Number = rand($Start, $End);
+            if(strlen($Number) != $Length){
+                $Number = $this->generateRandom($Start, $End, $Length); 
+            }
+            return $Number;
+        }
     }
